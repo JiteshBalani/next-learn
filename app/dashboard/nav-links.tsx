0fb25be146d27879823f56535a47126f1cd410d3
JiteshBalani <balani.jitesh911@gmail.com>
2025-02-23 20:36:28 +0530

    Added links array in nav-links.

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   UserGroupIcon,
@@ -12,7 +12,25 @@ import clsx from "clsx";
 // ...
 
 export default function NavLinks() {
-    const pathname = usePathname();
+  const pathname = usePathname();
+  const links = [
+    {
+      name: "Home",
+      href: "/dashboard",
+      icon: HomeIcon,
+    },
+    {
+      name: "Invoices",
+      href: "/dashboard/invoices",
+      icon: DocumentDuplicateIcon,
+    },
+    {
+      name: "Customers",
+      href: "/dashboard/customers",
+      icon: UserGroupIcon,
+    },
+  ];
+
   return (
     <>
       {links.map((link) => {
@@ -22,11 +40,11 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                {
-                  'bg-sky-100 text-blue-600': pathname === link.href,
-                },
-              )}
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-sky-100 text-blue-600": pathname === link.href,
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
